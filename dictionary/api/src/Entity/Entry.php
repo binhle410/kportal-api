0dc9b2181e41f79dd81c5781dbe3e637a8f96a3c
binhle410 <binh@sunrise.vn>
2019-05-29 22:48:48 +0700

    DICT API: Entry TYPE

--- dictionary/api/src/Entity/Entry.php
+++ dictionary/api/src/Entity/Entry.php
@@ -21,7 +21,7 @@ use Symfony\Component\Serializer\Annotation\Groups;
 class Entry
 {
     const TYPE_VERB_INTRANSITIVE = 'INTRANSITIVE_VERB';
-
+    
     public function __construct()
     {
         $this->examples = new ArrayCollection();
